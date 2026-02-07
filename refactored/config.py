b9ffe0560399f5df92dcd74369ce95f5a34a6be6
HaltTheGrey <jessneug@amazon.com>
2026-02-07 06:06:30 -0800

    v1.2.6 - Working incremental update with exe download

--- refactored/config.py
+++ refactored/config.py
@@ -1,7 +1,7 @@
 """Application configuration and constants."""
 
 # Version
-APP_VERSION = "1.2.5"
+APP_VERSION = "1.2.6"
 UPDATE_CHECK_URL = "https://raw.githubusercontent.com/HaltTheGrey/lru-tracker/main/version.json"
 
 # File paths
